750387bf73549645d2c3f49819c1067cda98fc7f
Yukuo Cen <cenyk1230@qq.com>
2020-04-05 19:12:46 +0800

    modify test ner

--- test_ner.py
+++ test_ner.py
@@ -5,7 +5,25 @@ with BertClient(show_server_config=False, check_version=False, check_length=Fals
     start_t = time.perf_counter()
     str1 = '我有鸡蛋和黄瓜可以做什么？'
     str2 = '不辣的川菜。'
-    str3 = '我有鸡蛋'
-    rst = bc.encode([str1, str2, str3])
+    str3 = '推荐一个有香菇的菜'
+    strs = [str1, str2, str3]
+    rst = bc.encode(strs)
     print('rst:', rst)
+    for st, sen in zip(strs, rst):
+        tmp = []
+        flag = False
+        for i in range(len(sen)):
+            if sen[i] == 'B-ING':
+                tmp.append(st[i])
+                flag = True
+            elif sen[i] == 'I-ING':
+                if flag:
+                    tmp[-1] = tmp[-1] + st[i]
+                else:
+                    tmp.append(st[i])
+                    flag = True
+            else:
+                flag = False
+        print(tmp)
+
     print(time.perf_counter() - start_t)
--- train_ner.sh
+++ train_ner.sh
@@ -4,5 +4,6 @@ bert-base-ner-train \
     -init_checkpoint ./albert_tiny/bert_model.ckpt \
     -bert_config_file ./albert_tiny/bert_config.json \
     -vocab_file ./albert_tiny/vocab.txt \
-    -device_map 1 \
-    -batch_size 4 \
+    -device_map 0 \
+    -batch_size 16 \
+    -num_train_epoch 5 \
